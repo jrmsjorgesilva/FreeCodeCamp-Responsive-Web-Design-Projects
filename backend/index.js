b88b6d9c689983be3f09ba551056649d6c36231f
jorge machado <jrms_jorgesilva@hotmail.com>
2022-06-30 13:27:26 -0300

    implementação de backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,26 @@
 const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require("dotenv").config();
+
+const app = express();
+
+const firebaseConfigProtected = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.json(firebaseConfigProtected);
+})
+
+app.listen(PORT, () => console.log(`server is serving on port ${PORT}`));
